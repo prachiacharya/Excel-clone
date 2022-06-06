f9bc90868dcf200debc080dd6c1d0927c8e2dad2
prachiacharya <82755292+prachiacharya@users.noreply.github.com>
2022-06-06 23:50:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,57 @@
+let topRow = document.querySelector(".top-row");
+let leftCol = document.querySelector(".left-col");
+let topLeftCell = document.querySelector(".top-left-cell");
+let allCells = document.querySelectorAll(".cell");
+let addressInput = document.querySelector("#address");
+let formulaInput = document.querySelector("#formula");
+let lastSelectedCell;
+
+cellsContentDiv.addEventListener("scroll", function(e) {
+    let scrollFromTop = e.target.scrollTop;
+    let scrollFromLeft = e.target.scrollLeft;
+    topRow.style.top = scrollFromTop + "px";
+    leftCol.style.left = scrollFromLeft + "px";
+    topLeftCell.style.top = scrollFromTop + "px";
+    topLeftCell.style.left = scrollFromLeft + "px";
+})
+
+for (let i = 0; i < allCells.length; i++) {
+    allCells[i].addEventListener("click", function(e) {
+        let rowId = Number(e.target.getAttribute("rowid"));
+        let colId = Number(e.target.getAttribute("colid"));
+        let address = String.fromCharCode(65 + colId) + (rowId + 1) + "";
+        // console.log(address);
+        let cellObject = db[rowId][colId];
+        addressInput.value = address;
+        //update UI
+        formulaInput.value = cellObject.formula;
+    })
+
+    allCells[i].addEventListener("blur", function(e) {
+        lastSelectedCell = e.target;
+        let cellValue = e.target.textContent;
+        let { rowId, colId } = getRowIdColIdFromElement(e.target);
+        let cellObject = db[rowId][colId];
+        if (cellObject.value == cellValue) {
+            return;
+        }
+        cellObject.value = cellValue;
+        console.log("After UPdate", cellObject);
+        updateChildren(cellObject);
+    })
+}
+
+
+formulaInput.addEventListener("blur", function(e) {
+    let formula = e.target.value;
+    if (formula) {
+        let { rowId, colId } = getRowIdColIdFromElement(lastSelectedCell);
+        let cellObject = db[rowId][colId];
+        let computedValue = solveFormula(formula, cellObject); // will implement in next commit
+        //update db
+        cellObject.value = computedValue;
+        cellObject.formula = formula;
+        //update ui
+        lastSelectedCell.textContent = computedValue;
+    }
+})
